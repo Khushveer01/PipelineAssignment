5636dfcc33d9b29b4964752f9fd4c734490ba2ae
Khushveer01 <130538249+Khushveer01@users.noreply.github.com>
2025-04-05 10:50:50 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,44 +1,29 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
   subscription_id = "4fc59329-05df-497c-b332-226931637540"
 }
 
-# Create a Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+resource "azurerm_resource_group" "this" {
+  name     = "rg-jenkins"
   location = var.location
 }
 
-# Create a Windows-based App Service Plan
-resource "azurerm_app_service_plan" "app_plan" {
-  name                = var.app_service_plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  kind                = "Windows"  
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+resource "azurerm_service_plan" "this" {
+  name                = "appserviceplan-jenkins"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  os_type             = "Linux"
+
+  sku_name = "B1"
 }
 
-# Create a Windows-based App Service for .NET 8
-resource "azurerm_app_service" "app_service" {
-  name                = var.app_service_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.app_plan.id
+resource "azurerm_linux_web_app" "this" {
+  name                = "Khushveer1103"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  service_plan_id     = azurerm_service_plan.this.id  # Updated attribute
 
   site_config {
-    always_on          = true
-    windows_fx_version = "DOTNET|8.0"  
+    always_on = true
   }
 }
